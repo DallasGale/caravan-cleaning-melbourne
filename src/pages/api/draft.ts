335dacf9ba098b36ba5f64b0cd774a4f3fea318c
Dallas Gale <dallasgale.digital@gmail.com>
2024-06-28 09:25:33 +1000

    wip

--- src/pages/api/draft.ts
+++ src/pages/api/draft.ts
@@ -1,48 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { isValidSecret } from 'sanity-plugin-iframe-pane/is-valid-secret'
 
-import { previewSecretId, readToken } from '~/lib/sanity.api'
-import { getClient } from '~/lib/sanity.client'
+import { projectId, dataset, apiVersion } from '~/lib/sanity.api'
+
+import { validatePreviewUrl } from '@sanity/preview-url-secret'
+import { createClient } from 'next-sanity'
+
+const token = process.env.SANITY_API_READ_TOKEN
+if (!token) {
+  throw new Error(
+    'A secret is provided but there is no `SANITY_API_READ_TOKEN` environment variable setup.',
+  )
+}
+const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: false,
+  token,
+})
 
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse<string | void>,
 ) {
-  if (!readToken) {
-    res.status(500).send('Misconfigured server')
-    return
+  if (!req.url) {
+    throw new Error('Missing url')
   }
-
-  const { query } = req
-
-  const secret = typeof query.secret === 'string' ? query.secret : undefined
-  const slug = typeof query.slug === 'string' ? query.slug : undefined
-
-  if (!secret) {
-    res.status(401)
-    res.send('Invalid secret')
-    return
-  }
-
-  const authClient = getClient(readToken).withConfig({
-    useCdn: true,
-    token: readToken,
-  })
-
-  // This is the most common way to check for auth, but we encourage you to use your existing auth
-  // infra to protect your token and securely transmit it to the client
-  const validSecret = await isValidSecret(authClient, previewSecretId, secret)
-  if (!validSecret) {
+  const { isValid, redirectTo = '/' } = await validatePreviewUrl(
+    client,
+    req.url,
+  )
+  if (!isValid) {
     return res.status(401).send('Invalid secret')
   }
-
-  if (slug) {
-    res.setDraftMode({ enable: true })
-    res.writeHead(307, { Location: `/post/${slug}` })
-    res.end()
-    return
-  }
-
-  res.status(404).send('Slug query parameter is required')
+  // Enable Draft Mode by setting the cookies
+  res.setDraftMode({ enable: true })
+  res.writeHead(307, { Location: redirectTo })
   res.end()
 }
