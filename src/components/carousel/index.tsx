498af241259c2b1abad53c1ffde11e3dba03de12
Dallas Gale <dallasgale.digital@gmail.com>
2024-07-11 13:19:32 +1000

    wip

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -30,7 +30,7 @@ const Carousel = ({ assets }: CarouselProps) => {
     className: 'center',
     centerMode: true,
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     centerPadding: '60px',
     swipeToSlide: true,
