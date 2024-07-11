f4daf67514b60ac2a83dc5b5f3ff4ab6fb5e9992
Dallas Gale <dallasgale.digital@gmail.com>
2024-07-11 17:21:49 +1000

    wip

--- src/components/global/contact/index.tsx
+++ src/components/global/contact/index.tsx
@@ -3,7 +3,7 @@ import PrimaryCta from '~/components/cta/primary'
 
 const Contact = () => {
   return (
-    <section className="contact">
+    <section className="contact" id="enquire">
       <div className="contact__container">
         <h1 className="heading-2 color-navy weight-bold">
           Contact us today for a FREE QUOTE.
