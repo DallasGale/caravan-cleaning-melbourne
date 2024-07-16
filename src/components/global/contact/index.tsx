351053b513967c4c6a61054922b82d3284901781
Dallas Gale <dallasgale.digital@gmail.com>
2024-07-16 16:56:18 +1000

    wip

--- src/components/global/contact/index.tsx
+++ src/components/global/contact/index.tsx
@@ -1,14 +1,41 @@
 import { Select, TextInput, Textarea } from '@mantine/core'
 import PrimaryCta from '~/components/cta/primary'
 import { useForm, ValidationError } from '@formspree/react'
+import emailjs from 'emailjs-com'
+import Swal from 'sweetalert2'
+import { useState } from 'react'
 
 const Contact = () => {
-  const [state, handleSubmit, reset] = useForm(process.env.NEXT_PUBLIC_FORM)
+  // const [state, handleSubmit, reset] = useForm(process.env.NEXT_PUBLIC_FORM)
 
+  const [result, setResult] = useState('')
+
+  const onSubmit = async (event) => {
+    event.preventDefault()
+    setResult('Sending....')
+    const formData = new FormData(event.target)
+
+    formData.append('access_key', process.env.NEXT_WEB3EMAIL_ACCESS_KEY)
+
+    const response = await fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      body: formData,
+    })
+
+    const data = await response.json()
+
+    if (data.success) {
+      setResult('Form Submitted Successfully')
+      event.target.reset()
+    } else {
+      console.log('Error', data)
+      setResult(data.message)
+    }
+  }
   return (
     <section className="contact" id="enquire">
       <div className="contact__container">
-        {state.submitting && <p>Submitting…</p>}
+        {/* {state.submitting && <p>Submitting…</p>}
         {state.succeeded && (
           <div>
             <p>Thanks!</p>
@@ -16,28 +43,37 @@ const Contact = () => {
           </div>
         )}
 
-        {!state.succeeded && (
-          <>
-            <h1 className="heading-2 color-navy weight-bold">
-              Contact us today for a FREE QUOTE.
-            </h1>
-            <p className="body-1 weight-semi">
-              Contact us now for a complimentary, no-obligation quote tailored
-              to your specific caravan and motorhome cleaning needs.
-            </p>
-            <form
-              onSubmit={handleSubmit}
-              className="contact__form"
-              method="POST"
-            >
-              <TextInput
-                name="name"
-                id="name"
-                placeholder="First Name"
-                classNames={{ input: 'form__fields' }}
-              />
+        {!state.succeeded && ( */}
+        <>
+          <h1 className="heading-2 color-navy weight-bold">
+            Contact us today for a FREE QUOTE.
+          </h1>
+          <p className="body-1 weight-semi">
+            Contact us now for a complimentary, no-obligation quote tailored to
+            your specific caravan and motorhome cleaning needs.
+          </p>
+          <form onSubmit={onSubmit}>
+            <input
+              type="hidden"
+              name="subject"
+              value="New Submission from Web3Forms"
+            />
 
-              <ValidationError
+            <TextInput
+              required
+              name="firstNAME"
+              id="firstNAME"
+              placeholder="First Name"
+              classNames={{ input: 'form__fields' }}
+            />
+            <TextInput
+              required
+              name="lastNAME"
+              id="lastNAME"
+              placeholder="Last Name"
+              classNames={{ input: 'form__fields' }}
+            />
+            {/* <ValidationError
                 prefix="Name"
                 field="name"
                 errors={state.errors}
@@ -80,14 +116,11 @@ const Contact = () => {
                 placeholder="Leave a message"
                 rows={8}
                 classNames={{ input: 'form__fields' }}
-              />
-
-              <button type="submit" disabled={state.submitting}>
-                Submit
-              </button>
-            </form>
-          </>
-        )}
+              /> */}
+            <button type="submit">Submit</button>
+          </form>
+        </>
+        {/* )} */}
       </div>
     </section>
   )
