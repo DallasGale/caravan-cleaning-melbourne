b0e1b65de180a905758eb8ae7a70e3e04fa21dee
Dallas Gale <dallasgale.digital@gmail.com>
2024-07-30 20:24:04 +1000

    wip

--- src/components/carousel/comparison.tsx
+++ src/components/carousel/comparison.tsx
@@ -3,6 +3,7 @@ import { SlidePairTypes } from '~/lib/sanity.queries'
 import { useEffect, useRef, useState } from 'react'
 
 import SlideContent from './slideContent'
+import { useIsMobile } from '~/hooks/useIsMobile'
 
 interface ComparisonCarouselProps {
   category: string
@@ -13,55 +14,70 @@ const ComparisonCarousel = ({
   category,
   slidePairs,
 }: ComparisonCarouselProps) => {
-  // const sliderRef = useRef<Slider>(null)
-  // const [assetsLoaded, setAssetsLoaded] = useState(0)
-  // const totalAssets = slidePairs?.length || 0
-
-  // useEffect(() => {
-  //   if (assetsLoaded === totalAssets && sliderRef.current) {
-  //     sliderRef.current.slickGoTo(0)
-  //   }
-  // }, [assetsLoaded, totalAssets])
-
-  // const handleAssetLoad = () => {
-  //   setAssetsLoaded((prev) => prev + 1)
-  // }
-
   const settings = {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: false,
     swipeToSlide: true,
     speed: 350,
+    infinite: true,
     dots: true,
     variableWidth: false,
   }
 
+  const [activeSlideIndex, setActiveSlideIndex] = useState<number>(0)
+  const isMobile = useIsMobile()
+  const flattenSlidePairs = (slidePairs) => {
+    return slidePairs.flatMap(({ caption, beforeItem, afterItem, _key }) => [
+      { ...beforeItem, caption, _key: `${_key}-before` },
+      { ...afterItem, caption, _key: `${_key}-after` },
+    ])
+  }
+  const items = isMobile ? flattenSlidePairs(slidePairs) : slidePairs
+
+  console.log({ items })
   return (
     <div className="slider-container">
       <h3 className="heading-3 color-white">{category}</h3>
-      <Slider {...settings} lazyLoad="ondemand">
-        {slidePairs.map(({ caption, beforeItem, afterItem, _key }, index) => {
-          return (
-            <div key={_key} className="slide-wrapper">
-              {/* Before slide */}
-              <SlideContent
-                item={beforeItem}
-                caption={caption}
-                label="Before"
-                // onLoad={handleAssetLoad}
-              />
-              {/* After slide */}
-              <SlideContent
-                item={afterItem}
-                caption={caption}
-                label="After"
-                // onLoad={handleAssetLoad}
-              />
-            </div>
-          )
-        })}
+      <Slider
+        className="slider"
+        {...settings}
+        lazyLoad="ondemand"
+        afterChange={(e) => setActiveSlideIndex(e)}
+      >
+        {isMobile &&
+          flattenSlidePairs(slidePairs).map((item, { _key }, index) => {
+            return (
+              <div key={_key} className="slide-wrapper">
+                <SlideContent item={item} />
+              </div>
+            )
+          })}
+        {!isMobile &&
+          slidePairs.map(({ beforeItem, afterItem, _key }, index) => {
+            return (
+              <div key={_key} className="slide-wrapper">
+                {beforeItem && <SlideContent item={beforeItem} />}
+                {afterItem && <SlideContent item={afterItem} />}
+              </div>
+            )
+          })}
       </Slider>
+      {!isMobile && (
+        <>
+          <div className="carousel__title-before">
+            <p className="display-2 color-white">Before</p>
+          </div>
+          <div className="carousel__title-after">
+            <p className="display-2 color-white">After</p>
+          </div>
+        </>
+      )}
+      <div className="slide-caption">
+        {items[activeSlideIndex] && (
+          <p className="paragraph">{items[activeSlideIndex].caption}</p>
+        )}
+      </div>
     </div>
   )
 }
--- src/components/carousel/slideContent.tsx
+++ src/components/carousel/slideContent.tsx
@@ -13,41 +13,21 @@ interface SlideContentProps {
     }
     youTubeId?: string
   }
-  caption: string
-  label: string
-  // onLoad: () => void
-  // priority?: boolean
 }
-const SlideContent: React.FC<SlideContentProps> = ({
-  item,
-  caption,
-  label,
-  // onLoad,
-  // priority = false,
-}) => {
+const SlideContent: React.FC<SlideContentProps> = ({ item }) => {
   return (
     <div className="slide">
-      <div className="slide-label">
-        <h3 className="display-3 color-white">{label}.</h3>
-      </div>
       {item.image && (
         <Image
-          // loading={priority ? 'eager' : 'lazy'}
           className="slider-image"
           src={item.image.asset.url}
           alt={item.image.imageAlt}
           layout="responsive"
           width={400}
           height={600}
-          // onLoad={onLoad}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          // priority={priority}
         />
       )}
       {item.youTubeId && <YouTubePlayer id={item.youTubeId} />}
-      <div className="slide-caption">
-        <p className="display-3">{caption}</p>
-      </div>
     </div>
   )
 }
